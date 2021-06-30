f3093ea749f7c82fa72d2d2302f3b984f20e9274
Deej <deejpotter@gmail.com>
2021-06-30 16:24:38 +1000

    Fixed images and css

--- .eleventy.js
+++ .eleventy.js
@@ -57,8 +57,8 @@ module.exports = function(eleventyConfig) {
   });
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
